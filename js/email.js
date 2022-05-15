0ea5fbf88a26d8067af7cbc8158c038f13bd10f5
saivra <info@saivra.com>
2022-05-15 18:50:36 +0500

    email done finaly

--- js/email.js
+++ js/email.js
@@ -1,7 +1,7 @@
 function sendEmail(){
     Email.send({
     SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
-    To : 'saivra.official@gmail.com',
+    To : 'rrstekshop@gmail.com',
     From : "info@rrsteksecurity.com",
     Subject : "New Contact Form Enquiry",
     Body : " <h3> First Name:  " + document.getElementById("first-name").value
