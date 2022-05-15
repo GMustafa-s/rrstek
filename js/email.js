2b8b478abbff7327c800988635a856140e8d0f39
saivra <info@saivra.com>
2022-05-15 18:12:41 +0500

    email funcitonality tested and done

--- js/email.js
+++ js/email.js
@@ -1,13 +1,17 @@
 function sendEmail(){
     Email.send({
-        Host : "smtp.gmail.com",
-        Username : "saivra.official@gmail.com",
-        Password : "03420810055MSkhan",
-        To : 'saivra.official@gmail.com',
-        From : "info@rrsteksecurity.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+    SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
+    To : 'curingdisease@gmail.com',
+    From : "info@rrsteksecurity.com",
+    Subject : "New Contact Form Enquiry",
+    Body : "First Name:  " + document.getElementById("first-name").value
+    + "<br><br> Last Name:  " + document.getElementById("last-name").value
+    + "<br><br> Email:  " + document.getElementById("email").value
+    + "<br><br> Country / Region:  " + document.getElementById("region").value
+    + "<br><br> Message:  " + document.getElementById("message").value
+    
     }).then(
-      message => alert(message)
+    message => alert(message)
     );
-}
+    }
+    
