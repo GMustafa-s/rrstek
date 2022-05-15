e0b1364a173886ee539ab6bdf587be386e512564
saivra <info@saivra.com>
2022-05-15 18:22:49 +0500

    email styling

--- js/email.js
+++ js/email.js
@@ -1,17 +1,17 @@
 function sendEmail(){
     Email.send({
     SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
-    To : 'curingdisease@gmail.com',
+    To : 'saivra.official@gmail.com',
     From : "info@rrsteksecurity.com",
     Subject : "New Contact Form Enquiry",
-    Body : "First Name:  " + document.getElementById("first-name").value
-    + "<br><br> Last Name:  " + document.getElementById("last-name").value
+    Body : " <h3> First Name:  " + document.getElementById("first-name").value
+    + "<br><br>  Last Name:  "  + document.getElementById("last-name").value
     + "<br><br> Email:  " + document.getElementById("email").value
     + "<br><br> Country / Region:  " + document.getElementById("region").value
     + "<br><br> Message:  " + document.getElementById("message").value
     
     }).then(
-    message => alert(message)
+    message => alert("Your message sent successfully")
     );
     }
     
