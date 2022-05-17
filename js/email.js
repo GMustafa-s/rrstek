2c6af5f3b9c106b095705b4958ec99bfa0f22da1
saivra <info@saivra.com>
2022-05-17 20:41:42 +0500

    Updates working email to info@rrsteksecurity.com

--- js/email.js
+++ js/email.js
@@ -2,35 +2,46 @@
 function sendEmail(){
     Email.send({
         SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
-        To : 'contact@rrsteksecurity.com',
-        cc : "curingdisease@gmail.com",
-        ReplyTo: 'contact@rrsteksecurity.com',
-        From : 'info@rrsteksecurity.com',
+        To : 'info@rrsteksecurity.com',
+        ReplyTo: document.getElementById("email").value,
+        From : 'contact@rrsteksecurity.com',
         Subject : "New Contact Form Enquiry",
-        Body : " <h3> First Name:  " + document.getElementById("first-name").value
+        Body :"<h3>Contact Form Query</h3>"+"<br>You got a query from your costumers, Please Contact them back as soon as possible"+"<br><br> First Name:  " + document.getElementById("first-name").value
         + "<br><br>  Last Name:  "  + document.getElementById("last-name").value
         + "<br><br> Email:  " + document.getElementById("email").value
         + "<br><br> Country / Region:  " + document.getElementById("region").value
         + "<br><br> Message:  " + document.getElementById("message").value
         
         }).then(
-        message => alert(message + "Your message sent successfully")
+        message => alert(message + "!, Your message sent successfully")
     );
 }
     
 
-
     function demoEmail(){
         Email.send({
             SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
-            To : 'rrstechshop@gmail.com',
-            // cc : 'rrstechshop@gmail.com',
-            cc : 'curingdisease@gmail.com',
-            From : document.getElementById("email").value,
+            To : 'info@rrsteksecurity.com',
+            ReplyTo: document.getElementById("email").value,
+            From : 'contact@rrsteksecurity.com',
             Subject : "New Contact Form Enquiry",
-            Body : " <h3> Your Name:  " + document.getElementById("name").value
+            Body : "<h3>Contact Form Query</h3><br>You got a query from your costumers, Please Contact them back as soon as possible"+"<br><br>Your Name:  " + document.getElementById("name").value
             + "<br><br>  Your Email:  "  + document.getElementById("email").value 
             }).then(
-            message => alert(message + "Your message sent successfully")
+            message => alert(message + "! Your message sent successfully")
+        );
+    }
+
+    function demoInstall(){
+        Email.send({
+            SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
+            To : 'info@rrsteksecurity.com',
+            From : 'contact@rrsteksecurity.com',
+            Subject : "New Contact Form Enquiry",
+            Body : "<h3>Contact Form Query</h3><br>You got a query from your costumers, Please Contact them back as soon as possible"
+            +"<br><br>Your Name:  " + document.getElementById("name").value
+            + "<br><br>  Phone:  "  + document.getElementById("phone").value 
+            }).then(
+            message => alert(message + "! Your message sent successfully")
         );
     }
