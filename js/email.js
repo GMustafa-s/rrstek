9870ee4cb5654873585043500284bb619a89385b
saivra <info@saivra.com>
2022-05-17 14:03:57 +0500

    update

--- js/email.js
+++ js/email.js
@@ -2,9 +2,10 @@
 function sendEmail(){
     Email.send({
         SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
-        To : 'info@rrsteksecurity.com',
-        cc : 'rrstechshop@gmail.com'+'mustafa.gdesigner@gmail.com',
-        From : "info@rrsteksecurity.com",
+        To : 'contact@rrsteksecurity.com',
+        cc : "curingdisease@gmail.com",
+        ReplyTo: 'contact@rrsteksecurity.com',
+        From : 'info@rrsteksecurity.com',
         Subject : "New Contact Form Enquiry",
         Body : " <h3> First Name:  " + document.getElementById("first-name").value
         + "<br><br>  Last Name:  "  + document.getElementById("last-name").value
@@ -13,7 +14,7 @@ function sendEmail(){
         + "<br><br> Message:  " + document.getElementById("message").value
         
         }).then(
-        message => alert("Your message sent successfully")
+        message => alert(message + "Your message sent successfully")
     );
 }
     
@@ -22,13 +23,14 @@ function sendEmail(){
     function demoEmail(){
         Email.send({
             SecureToken: "2e489077-907b-4fba-b87e-9905e749bb29",
-            To : 'info@rrsteksecurity.com',
-            cc : 'rrstechshop@gmail.com'+'mustafa.gdesigner@gmail.com',
-            From : "info@rrsteksecurity.com",
+            To : 'rrstechshop@gmail.com',
+            // cc : 'rrstechshop@gmail.com',
+            cc : 'curingdisease@gmail.com',
+            From : document.getElementById("email").value,
             Subject : "New Contact Form Enquiry",
             Body : " <h3> Your Name:  " + document.getElementById("name").value
             + "<br><br>  Your Email:  "  + document.getElementById("email").value 
             }).then(
-            message => alert("Your message sent successfully")
+            message => alert(message + "Your message sent successfully")
         );
     }
